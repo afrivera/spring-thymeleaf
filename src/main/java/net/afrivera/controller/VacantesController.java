4a894501c130abc3408cb27968e82e21f321533b
afrivera <andresrfelip@hotmail.com>
2022-09-23 21:20:26 -0500

    agregando metodo para eliminar y visualizar campos en la vista tabla para poder interactuar con detalle y eliminar

--- src/main/java/net/afrivera/controller/VacantesController.java
+++ src/main/java/net/afrivera/controller/VacantesController.java
@@ -5,16 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
 
+    @GetMapping("/delete")
+    public String eliminar(@RequestParam("id") int idVacante, Model model){
+        model.addAttribute("idVacante", idVacante);
+        System.out.println("borrando vacante con id = " + idVacante);
+        return "mensaje";
+    }
+
     @GetMapping("/view/{id}")
     public String verDetalle(@PathVariable("id") int idVacante, Model model){
         model.addAttribute("idVacante", idVacante);
         System.out.println("id vacante: " + idVacante);
-        //TODO: Buscar los detalles de la vacante
+        //TODO: Buscar los detalles de la vacante en la bd
         return "vacantes/detalle";
     }
 }

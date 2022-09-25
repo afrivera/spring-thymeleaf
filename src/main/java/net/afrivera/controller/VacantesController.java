ca7bd07aad7b31bb6f0b72aaf9e2e972ea903d7f
afrivera <andresrfelip@hotmail.com>
2022-09-24 22:10:34 -0500

    maquetacion de formulario vacantes creación de metodo post para recibir la info del formulario

--- src/main/java/net/afrivera/controller/VacantesController.java
+++ src/main/java/net/afrivera/controller/VacantesController.java
@@ -5,10 +5,7 @@ import net.afrivera.service.IVacanteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/vacantes")
@@ -17,6 +14,30 @@ public class VacantesController {
     @Autowired
     private IVacanteService serviceVacante;
 
+    @PostMapping("/save")
+    public String guardar(
+            @RequestParam("nombre") String nombre,
+            @RequestParam("descripcion") String descripcion,
+            @RequestParam("estatus") String estatus,
+            @RequestParam("fecha") String fecha,
+            @RequestParam("destacado") int destacado,
+            @RequestParam("salario") double salario,
+            @RequestParam("detalles") String detalles
+            ){
+        System.out.println("nombre = " + nombre);
+        System.out.println("descripcion = " + descripcion);
+        System.out.println("estatus = " + estatus);
+        System.out.println("fecha = " + fecha);
+        System.out.println("destacado = " + destacado);
+        System.out.println("salario = " + salario);
+        System.out.println("detalles = " + detalles);
+
+        return "vacantes/listVacantes";
+    }
+    @GetMapping("/create")
+    public String crear(){
+        return "vacantes/formVacante";
+    }
     @GetMapping("/delete")
     public String eliminar(@RequestParam("id") int idVacante, Model model){
         model.addAttribute("idVacante", idVacante);
